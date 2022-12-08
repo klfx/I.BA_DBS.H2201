c0f323b494774c7ab0c1b316d7c02bf9b84792ed
Flavio Kluser <flavio.kluser@stud.hslu.ch>
2022-12-08 17:11:07 +0100

    extended query_ideas.sql

--- code_requests_visualisations/query_ideas.sql
+++ code_requests_visualisations/query_ideas.sql
@@ -2,9 +2,9 @@
 7. Anfragen umsetzen
 
 [X] Verbinden mind. zweier Entitätsmengen (Join)
-[] Verarbeiten von Wertemengen zu einem Wert (Aggregat)
-[] Gruppierung von Aggregaten nach mind. Einer Dimension (SQL: group by)
-[] Datensätze filtern (Selektion), Attribute auswählen (Projektion)
+[X] Verarbeiten von Wertemengen zu einem Wert (Aggregat)
+[X] Gruppierung von Aggregaten nach mind. Einer Dimension (SQL: group by)
+[X] Datensätze filtern (Selektion), Attribute auswählen (Projektion)
 [] Verschachtelung von Queries und Subqueries (Unterabfragen)
 */
 
@@ -20,3 +20,26 @@ select r.rating_id,m.title,r.rating,r.rating_date from ratings as r inner join m
 
 #Find Films with most ratings
 select count(r.movieId),r.movieId,m.title from ratings as r inner join movies as m on r.movieId=m.movieId group by m.movieId order by count(*) desc;
+
+#get mean ratings (1h runtime!!)
+select avg(rating),count(rating),movieId from ratings group by movieId;
+
+#mean ratings but with title(too time expensive)
+#create view mean_ratings as select avg(r.rating),count(r.rating),r.movieId,m.title from ratings as r inner join movies as m on r.movieId=m.movieId group by r.movieId;
+#select * from mean_ratings;
+
+#Zählen der Genres bei Filmen mit >= 6 Genres (AGGREGAT, GROUP BY, SELEKTION, PROJEKTION)
+select m.movieId,m.title,count(mg.genreId) as 'Amount genres' from movies as m inner join movies_genres as mg on m.movieId=mg.movieId group by m.movieId having count(mg.genreId) >= 6;
+
+DELIMITER $$
+CREATE PROCEDURE popular_movies.search_by_tags(IN tag varchar(30))
+BEGIN
+	SET @tag = tag;
+    SET @searchstring = concat('%',@tag,'%');
+	SELECT t.movieId,t.tag,m.title,count(t.movieId) FROM tags as t inner join movies as m on t.movieId=m.movieId where t.tag like "@searchstring" group by t.movieId order by count(t.movieId) desc;
+END$$
+DELIMITER;
+
+drop procedure popular_movies.search_by_tags;
+
+Call popular_movies.search_by_tags('action');
