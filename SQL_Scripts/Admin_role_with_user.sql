e0d525e22badc09eae4ca9fbfc24939ef7de920c
Flavio Kluser <flavio.kluser@stud.hslu.ch>
2022-12-08 13:42:50 +0100

    fix admin user script

--- SQL_Scripts/Admin_role_with_user.sql
+++ SQL_Scripts/Admin_role_with_user.sql
@@ -5,9 +5,10 @@ CREATE USER 'fabien_morgan'@'%' IDENTIFIED BY '';
 CREATE USER 'flavio_kluser'@'%' IDENTIFIED BY '';
 
 
-GRANT admin TO 'fabien_morgan'@'%';
-GRANT admin TO 'flavio_kluser'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'fabien_morgan'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'flavio_kluser'@'%' WITH GRANT OPTION;
 
 SET DEFAULT ROLE ALL TO 'fabien_morgan'@'%';
 SET DEFAULT ROLE ALL TO 'flavio_kluser'@'%';
 
+FLUSH PRIVILEGES;
